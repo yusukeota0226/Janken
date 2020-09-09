0674e7c47c3ea6a911f383e37024ebece4d68c53
yuota <elle.226.725@gmail.com>
2020-09-09 23:48:32 +0900

    チョキ動作不具合解消

--- app/src/main/java/com/example/janken/MainActivity.kt
+++ app/src/main/java/com/example/janken/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.janken
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gu.setOnClickListener { onJankenButtonTapped(it)}
+        choki.setOnClickListener { onJankenButtonTapped(it)}
+        pa.setOnClickListener { onJankenButtonTapped(it)}
+    }
+
+    fun onJankenButtonTapped(view: View?){
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("MY_HAND", view?.id)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/janken/ResultActivity.kt
+++ app/src/main/java/com/example/janken/ResultActivity.kt
@@ -2,10 +2,52 @@ package com.example.janken
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
+
+    val gu = 0
+    val choki = 1
+    val pa = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        val id = intent.getIntExtra("MY_HAND", 0)
+
+        val myHand: Int
+        myHand = when(id){
+            R.id.gu -> {
+                myHandImage.setImageResource(R.drawable.gu)
+                gu
+            }
+            R.id.choki -> {
+                myHandImage.setImageResource(R.drawable.choki)
+                choki
+            }
+            R.id.pa -> {
+                myHandImage.setImageResource(R.drawable.pa)
+                pa
+            }
+            else -> gu
+        }
+
+        //コンピュータの手を決める
+        val comHand = (Math.random() * 3).toInt()
+        when(comHand){
+            gu -> comHandImage.setImageResource(R.drawable.com_gu)
+            choki -> comHandImage.setImageResource(R.drawable.com_choki)
+            pa -> comHandImage.setImageResource(R.drawable.com_pa)
+        }
+
+        //勝敗を判定する
+        val gameResult = (comHand - myHand + 3) % 3
+        when(gameResult){
+            0 -> resultLabel.setText(R.string.result_draw) //引き分け
+            1 -> resultLabel.setText(R.string.result_win) //買った場合
+            2 -> resultLabel.setText(R.string.result_lose) //負けた場合
+        }
+
+        backButton.setOnClickListener { finish() }
     }
 }
